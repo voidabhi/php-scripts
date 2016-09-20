5896fc6ec1de56e6ddb052e46a0a174fe36b4981
Abhijeet Mohan <void.aby@gmail.com>
2016-09-20 22:01:48 +0530

    Add Imgur: Download all the images from imgur gallery

--- Imgur.php
+++ Imgur.php
@@ -1,1 +1,19 @@
 
+<?php
+// ID of gallery, you get it from URL.
+$galleryId = 'nuAM3';
+$url = 'http://imgur.com/ajaxalbums/getimages/'.$galleryId.'/hit.json?all=true';
+$jsonContent = file_get_contents($url);
+$data = json_decode($jsonContent, true);
+if(!is_dir('images_'.$galleryId)) {
+  mkdir('images_'.$galleryId);
+}
+foreach($data['data']['images'] as $image) {
+  
+  $name = $image['hash'].$image['ext'];
+  
+  $imageContent = file_get_contents('http://i.imgur.com/'.$name);
+  
+  file_put_contents('images_'.$galleryId.'/'.$name, $imageContent);
+}
+  
